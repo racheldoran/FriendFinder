569bd07f64c5e97d19c81d6061d2db801ff5497a
rachel doran <rmitchell0925@gmail.com>
2019-10-16 13:24:56 -0400

    still don't work yall

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
 
 var express = require("express");
 var path = require("path");
+var bodyParser = require("body-parser");
 
 var app = express();
-
 var PORT = process.env.PORT || 8080;
-
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
-
-
 
 require("/routing/apiRoutes.js")(app);
 require("/routing/htmlRoutes.js")(app);
