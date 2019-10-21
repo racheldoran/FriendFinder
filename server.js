f8a9b162b24d614f59279e2f49371b6af219ebba
rachel doran <rmitchell0925@gmail.com>
2019-10-21 17:41:01 -0400

    ughhhh

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 
 var express = require("express");
-var path = require("path");
 var bodyParser = require("body-parser");
 
 var app = express();
@@ -10,8 +9,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 
-require("/routing/apiRoutes.js")(app);
-require("/routing/htmlRoutes.js")(app);
+require("routing/apiRoutes.js")(app);
+require("routing/htmlRoutes.js")(app);
+
 
 app.listen(PORT, function() {
 	console.log("App listening on PORT: " + PORT);
