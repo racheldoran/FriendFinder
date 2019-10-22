be4b9cdf69e679e0c0e5a571fe94cb925cea8cc6
rachel doran <rmitchell0925@gmail.com>
2019-10-22 18:32:04 -0400

    IT WORKS AHHHHHHHH

--- FriendFinder/app/data/friends.js
+++ FriendFinder/app/data/friends.js
@@ -1,23 +1,23 @@
 var friendList = [
 	{
-		name: 'Dylan',
-		photo: '',
+		name: 'Mac',
+		photo: 'https://www.thewrap.com/wp-content/uploads/2011/08/Fat_Mac.jpg',
 		scores: ["3","5","5","3","2","3","5","3","2","1"],
 
-		name: 'Dylan',
-		photo: '',
+		name: 'Charlie',
+		photo: 'https://i.kym-cdn.com/entries/icons/facebook/000/022/524/tumblr_o16n2kBlpX1ta3qyvo1_1280.jpg',
 		scores: ["2","5","5","3","3","4","5","4","2","4"],
 
-		name: 'Dylan',
-		photo: '',
+		name: 'Dennis',
+		photo: 'https://i.pinimg.com/originals/ec/b2/e2/ecb2e2ac253463464b30d3e79e2fa221.jpg',
 		scores: ["5","4","3","2","3","5","1","2","3","1"],
 
-		name: 'Dylan',
-		photo: '',
+		name: 'Frank',
+		photo: 'https://static2.srcdn.com/wordpress/wp-content/uploads/2019/02/Danny-DeVito-on-Its-Always-Sunny-in-Philadelphia.jpg',
 		scores: ["1","2","3","4","2","5","2","1","5","1"],
 
-		name: 'Dylan',
-		photo: '',
+		name: 'Dee',
+		photo: 'https://bloximages.newyork1.vip.townnews.com/dailynebraskan.com/content/tncms/assets/v3/editorial/3/c5/3c55637a-6087-11e3-b6b3-0019bb30f31a/52a541c42d887.image.jpg',
 		scores: ["2","3","5","4","5","1","5","3","4","5"],
 
 	}
